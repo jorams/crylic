6cd59786d2dfe39eb6b6c52ad43951cbda05d65c
Joram Schrijver <i@joram.io>
2015-10-13 02:20:14 +0200

    Change end-of-string anchor to \Z
    It should've been changed in the transition from ^ to \A.

--- src/regex-lexer.lisp
+++ src/regex-lexer.lisp
@@ -115,7 +115,7 @@ and/or entering a new state."
                            ;; Because of the following two rules every state
                            ;; automatically stops at the end of the file, and
                            ;; all errors (when no rule matches) are caught.
-                           '(("$" :state :pop!)
+                           '(("\\Z" :state :pop!)
                              ("." :token :error)))
               for let-sym = (gensym "RULE-REGEX")
               if (eq :include regex)
